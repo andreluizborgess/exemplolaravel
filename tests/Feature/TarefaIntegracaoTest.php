586222366cadd15bba206fe14e89d58ebb3c7c13
andreluizborgess <andreluizborges1995@gmail.com>
2024-10-08 09:37:52 -0300

    0810

--- tests/Feature/TarefaIntegracaoTest.php
+++ tests/Feature/TarefaIntegracaoTest.php
@@ -17,21 +17,22 @@ class TarefaIntegracaoTest extends TestCase
     public function test_criar_tarefa_por_meio_da_api()
     {
         $response = $this->postJson('/api/tarefas', [
-            'titulo' => 'nova tarefa',
-            'descricao' => 'descricao da nova tarefa',
+            'titulo' => 'Nova tarefa',
+            'descricao' => 'Descrição da Nova tarefa',
             'concluida' => false,
         ]);
         $response->assertStatus(201)
             ->assertJson([
-                'titulo' => 'nova tarefa',
-                'descricao' => 'descricao da nova tarefa',
+                'titulo' => 'Nova tarefa',
+                'descricao' => 'Descrição da Nova tarefa',
                 'concluida' => false,
             ]);
     }
-    public function test_criar_tarefa_validar_required(){
-        $response = $this->postJson('api/tarefas',[
-'descricao' => 'Descrição da nova tarefa',
-'concluida' => false,
+    public function test_criar_tarefa_validar_required()
+    {
+        $response = $this->postJson('api/tarefas', [
+            'descricao' => 'Descrição da Nova tarefa',
+            'concluida' => false,
         ]);
         $response->assertStatus(422);
         $response->assertJsonValidationErrors(['titulo']);
